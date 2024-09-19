c8e960ede166836a778c8f980cec47649030e3e3
hemant mudgil <53985374+hemant-mudgil@users.noreply.github.com>
2024-09-19 10:50:20 +0530

    Update ubuntu_prod.sh

--- ubuntu_prod.sh
+++ ubuntu_prod.sh
@@ -232,7 +232,7 @@ pip3 install numpy pandas matplotlib requests beautifulsoup4 flask django
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
